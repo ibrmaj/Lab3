f8807d3641cb96bb2ffe2eecadd4a8f1d9e4fa12
Ismail <ismail.majid@mail.utoronto.ca>
2024-09-27 03:34:00 +0400

    checkstyle errors fixed in countrycodeconverter and InLabByHandTranslator

--- src/main/java/org/translation/InLabByHandTranslator.java
+++ src/main/java/org/translation/InLabByHandTranslator.java
@@ -21,17 +21,15 @@ public class InLabByHandTranslator implements Translator {
      * @param country the country
      * @return list of language abbreviations which are available for this country
      */
+    public static final String CANADA = "can";
+
     @Override
     public List<String> getCountryLanguages(String country) {
-        // TODO Checkstyle: The String "can" appears 4 times in the file.
-        if ("can".equals(country)) {
+        if (country.equals(CANADA)) {
             return new ArrayList<>(List.of("de", "en", "zh"));
         }
         return new ArrayList<>();
     }
-
-    // TODO Checkstyle: Static variable definition in wrong order.
-    public static final String CANADA = "can";
 
     /**
      * Returns the country abbreviations for all countries whose translations are
@@ -53,22 +51,23 @@ public class InLabByHandTranslator implements Translator {
      */
     @Override
     public String translate(String country, String language) {
-        // TODO Checkstyle: Return count is 5 (max allowed for non-void methods/ lambdas is 2).
-        // TODO Checkstyle: String literal expressions should be on the left side of an equals comparison
-        if (!country.equals("can")) {
-            return null;
+        String ans;
+
+        if (!CANADA.equals(country)) {
+            ans = null;
         }
-        if (language.equals("de")) {
-            return "Kanada";
+        if ("de".equals(language)) {
+            ans = "Kanada";
         }
-        else if (language.equals("en")) {
-            return "Canada";
+        else if ("en".equals(language)) {
+            ans = "Canada";
         }
         else if ("zh".equals(language)) {
-            return "加拿大";
+            ans = "加拿大";
         }
         else {
-            return null;
+            ans = null;
         }
+        return ans;
     }
 }
